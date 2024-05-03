e7302775a203d1b7ebf100f76159648402bdcc13
Sharath <sharathj116@gmail.com>
2024-05-03 23:21:05 +0530

    doctoe model created

---
+++ model/doctorModel.js
@@ -0,0 +1,20 @@
+const mongoose=require('mongoose')
+const Schema = new mongoose.Schema({
+    name: String,
+    specialization: String,
+    experience: Number,
+    fees: Number,
+    image: String,
+    description: String,
+    address: String,
+    timings: String,
+    phone: String,
+    email: String,
+    password: String,
+    isVerified: Boolean,
+    isBlocked: Boolean,
+
+
+    
+})
+module.exports = mongoose.model('Doctor', Schema)
